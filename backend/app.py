ec30bd0b7dabe22c505205303f0efd0242ed79ca
Rohan Kumar <rk399504@gmail.com>
2026-01-12 21:47:57 +0530

    add

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
     return response
 
 # Initialize extensions
